226a1c5b721aff23e9fe2da0512c08bf7d8187af
Kirill Pashovkin <kirillpashovkin@gmail.com>
2023-12-12 19:49:33 +0300

    HW-1

--- src/main/java/ru/mts/Main.java
+++ src/main/java/ru/mts/Main.java
@@ -2,6 +2,17 @@ package ru.mts;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        calculateTotalCost(new Purchase(3, 10, 0.75));
+        calculateTotalCost(new Purchase(5, 5.5, 42.575));
+        calculateTotalCost(new Purchase(10, 99.9, 59.1));
+    }
+
+    public static void calculateTotalCost(Purchase purchase){
+        String totalCostWithoutDiscount = String.format("%.2f", purchase.getCount() * purchase.getPrice());
+        String totalCostWithDiscount = String.format("%.2f", purchase.getCount()* purchase.getPrice() * (100 - purchase.getDiscount()) / 100);
+        System.out.println(purchase);
+        System.out.println("Общая стоимость без учёта скидки: " + totalCostWithoutDiscount);
+        System.out.println("Общая стоимость c учетом скидки: " + totalCostWithDiscount);
+        System.out.println();
     }
 }
